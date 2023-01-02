3ecc6360a794ea6d15c2973599b221ed96c60fff
MaryShtayn <106402872+MaryShtayn@users.noreply.github.com>
2023-01-02 22:56:14 +0100

    add js code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,43 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
-// import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 console.log(SimpleLightbox);
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryItem = document.querySelector('.gallery__item');
+const markup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+      onclick="return false"
+    />
+  </a>`
+    )
+    .join('');
+}
+gallery.insertAdjacentHTML('beforeend', markup);
+
+function onOpenModal(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+lightbox.on('show.simplelightbox', function () {
+  gallery.addEventListener('click', onOpenModal);
+});
