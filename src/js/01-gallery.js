0b99430fe6d30f73fb93569c13732ce513ead7dc
MaryShtayn <106402872+MaryShtayn@users.noreply.github.com>
2023-01-02 19:34:05 +0100

    add file

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,5 +3,5 @@ import SimpleLightbox from 'simplelightbox';
 import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+console.log(SimpleLightbox);
 console.log(galleryItems);
