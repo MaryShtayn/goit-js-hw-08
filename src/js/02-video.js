45b6d12d148daa5db521b360b2d798a420874ad5
MaryShtayn <106402872+MaryShtayn@users.noreply.github.com>
2023-01-07 16:31:27 +0100

    added js code to the task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,28 @@
 import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
+// console.log(Player);
 
-console.log(Player);
+const iframe = document.querySelector('#vimeo-player');
+const KEY_LOCAL = 'videoplayer-current-time';
+const player = new Player(iframe);
+
+function timeLap(data) {
+  const timeOff = JSON.stringify(data);
+  localStorage.setItem(KEY_LOCAL, timeOff);
+}
+player.on('timeupdate', throttle(timeLap, 1000));
+
+const timeOn = JSON.parse(localStorage.getItem(KEY_LOCAL));
+let timeMove = timeOn.seconds;
+
+player
+  .setCurrentTime(timeMove)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
